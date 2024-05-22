baf9b7a94652412e19f27f6a452380bdfdb68c83
Martin Synák <martinsynak@Martins-MacBook-Air.local>
2024-05-22 15:12:45 +0200

    Displaying questions with answers

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([]); 
+
+    const activeQuestion = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((previosAnswers) => {
+            return [...previosAnswers, selectedAnswer];
+        });
+    }
+
+    return(
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestion].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestion].answers.map( answer => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+        
+        
+    );
+}
